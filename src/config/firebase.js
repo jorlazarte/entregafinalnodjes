5848d2b71615ce9cda619dcaedfb930b0402553a
jorlazarte <jorgelazarte81@gmail.com>
2025-12-17 10:13:53 -0300

    agregar variables de firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import dotenv from 'dotenv';
+dotenv.config()
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCFh-MLt0qbnUGKPcEmL5n8tFWNOxVvxcQ",
-  authDomain: "apirest-nodejs.firebaseapp.com",
-  projectId: "apirest-nodejs",
-  storageBucket: "apirest-nodejs.firebasestorage.app",
-  messagingSenderId: "579855301223",
-  appId: "1:579855301223:web:59b46ff07b627bc71f32c2"
+  apiKey: process.env.FIREBASE_APIKEY, //"AIzaSyCFh-MLt0qbnUGKPcEmL5n8tFWNOxVvxcQ",
+  authDomain: process.env.FIREBASE_AUTHDOMAIN, //"apirest-nodejs.firebaseapp.com",
+  projectId: process.env.FIREBASE_PROJECTID, //"apirest-nodejs",
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET, //"apirest-nodejs.firebasestorage.app",
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENERDID, //"579855301223",
+  appId: process.env.FIREBASE_APPID //"1:579855301223:web:59b46ff07b627bc71f32c2"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 const db = getFirestore(app)
